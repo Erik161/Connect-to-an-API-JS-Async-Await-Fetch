521c9f100df9a13d1f0bcb03d2b221d976f6d327
Erik Hernandez <erikestuardo_161@hotmail.com>
2023-04-07 00:16:55 -0600

    We use the json() method to be able to access the information that is also asynchronous and therefore we have to add AWAIT.

--- main.js
+++ main.js
@@ -18,11 +18,14 @@ const cargarPeliculas= async() =>{
    -The AWAIT operator is used to wait for a PROMISE and can only be used within an ASYNC function.
    -It verifies the parameters you need to be able to make the request.
    -When we use Asynchronous functions, we should work with Try and catch to also be able to catch an error in the request.
+   -We use the json() method to be able to access the information that is also asynchronous and therefore we have to add AWAIT .
    */   
   
    try{
     const respuesta = await fetch('https://api.themoviedb.org/3/movie/550?api_key=be712d905546d6908c79fab28d8f4180&language=es-GT');
     console.log(respuesta);
+    const datos = await respuesta.json();
+    console.log(datos);
    }catch(error){
     console.log(error)
    }
